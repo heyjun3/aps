2054d16c4db3ff51133a376846d09ab61d52c69e
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-01-05 21:50:35 +0900

    update logging conf

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from ims import monthly
 
 
 if __name__ == '__main__':
-    logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'logging.conf'))
+    logging.config.fileConfig(os.path.join(os.path.dirname(__file__), 'logging.conf'), disable_existing_loggers=False)
     logger = logging.getLogger(__name__)
 
     monthly.main()
