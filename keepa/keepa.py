7bfef4b27f0103415c2dddc2f74386fbf8f29b7f
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-02-04 17:52:39 +0900

    fix models funcstion

--- keepa/keepa.py
+++ keepa/keepa.py
@@ -152,7 +152,4 @@ def keepa_worker():
                 pass
 
 
-def convert_keepa_time_to_datetime_date(keepa_time: int):
-    unix_time = (keepa_time + 21564000) * 60
-    date_time = datetime.datetime.fromtimestamp(unix_time)
-    return date_time.date()
+
