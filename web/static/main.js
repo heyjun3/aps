9a5192b54c0e1a29811702be010937ca9af5c7cb
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-02-06 16:36:01 +0900

    update chart.js

--- web/static/main.js
+++ web/static/main.js
@@ -1,29 +1,33 @@
-const ctx = document.getElementById('myChart');
+var drawChart = function (id, date, price, rank) {
+    const ctx = document.getElementById(id);
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: date,
+            datasets: [
+                {
+                    label: 'Price',
+                    data: price,
+                    // データライン
+                    borderColor: 'blue',
+                },
+                {
+                    label: 'Rank',
+                    data: rank,
+                    // データライン
+                    borderColor: 'green',
+                    yAxisID: 'y2',
+                },
+            ],
+        },
+        options: {
+            y2: {
+                position: 'right',
+            }
+        }
+    });
+}
 
-const chart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    }
-})
+var sample = function(){
+    console.log('test')
+}
--- web/web.py
+++ web/web.py
@@ -30,9 +30,24 @@ def delete_filename(filename):
     return redirect(url_for('index'))
 
 
-@app.route('/chart', methods=['GET'])
-def chart():
-    return render_template('chart_js.html')
+@app.route('/chart/<string:filename>', methods=['GET'])
+def chart(filename):
+    render_data_list = []
+    products_list = MWS.get_render_data(filename=filename)
+    for mws, keepa in products_list:
+        date, rank, price = keepa.render_price_rank_data_list
+        date = list(map(lambda x: x.isoformat(), date))
+        product = {
+            'title': mws.title,
+            'asin': mws.asin,
+            'date': date,
+            'price': price,
+            'rank': rank,
+            'jan': mws.jan,
+        }
+        render_data_list.append(product)
+
+    return render_template('chart_js.html', products_list=render_data_list)
 
 
 def start():
