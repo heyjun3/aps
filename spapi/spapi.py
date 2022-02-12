f2c33636e1c2009173d63c6f5309b6f816140f8d
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-02-13 01:33:33 +0900

    add get_price function

--- spapi/spapi.py
+++ spapi/spapi.py
@@ -2,6 +2,7 @@ import datetime
 import hmac
 import hashlib
 import json
+import os
 import time
 import urllib.parse
 
@@ -18,10 +19,11 @@ logger = log_settings.get_logger(__name__)
 ENDPOINT = 'https://sellingpartnerapi-fe.amazon.com'
 
 
-def request(method: str, url: str, params: dict = None, json: dict = None, headers: dict = None):
+def request(req: requests.Request) -> requests.Response:
     for _ in range(69):
         try:
-            response = requests.request(method, url=url, params=params, json=json, headers=headers)
+            session = requests.Session()
+            response = session.send(req.prepare())
             if response.status_code == 200 or response is not None:
                 return response
             else:
@@ -66,8 +68,9 @@ class SPAPI:
         'client_id': self.client_id,
         'client_secret': self.client_secret,
         }
+        req = requests.Request(method='POST', url=URL, params=params, headers=headers)
+        response = request(req)
 
-        response = request('POST', URL, params=params, headers=headers)
         access_token = response.json().get('access_token')
         
         if access_token is None:
@@ -76,45 +79,52 @@ class SPAPI:
 
         return access_token
 
-    def create_signature(self, method, canonical_uri, access_token: str, body, query: str = ''):
+    def create_authorization_headers(self, req: requests.Request) -> requests.Request:
         region = 'us-west-2'
         service = 'execute-api'
-        user_agent = 'My SPAPI Client tool /1.0(Language=python/3.10'
         algorithm = 'AWS4-HMAC-SHA256'
         signed_headers = 'host;user-agent;x-amz-access-token;x-amz-date'
 
         host = urllib.parse.urlparse(ENDPOINT).netloc
+        canonical_uri = urllib.parse.urlparse(req.url).path
+        user_agent = req.headers.get('user-agent')
+        body = ''
+
+        if  req.json:
+            body = json.dumps(req.json)
 
         t = datetime.datetime.utcnow()
         amz_date = t.strftime('%Y%m%dT%H%M%SZ')
         datestamp = t.strftime('%Y%m%d')
+        canonical_header_values = [host, user_agent, req.headers.get('x-amz-access-token'), f'{amz_date}\n']
+        
 
-        canonical_headers = 'host:' + host + '\n' + 'user-agent:' + user_agent + '\n' + 'x-amz-access-token:' + access_token + '\n' + 'x-amz-date:' + amz_date + '\n'
-        payload_hash = hashlib.sha256(json.dumps(body).encode('utf-8')).hexdigest()
+        canonical_headers = '\n'.join([f'{head}:{value}' for head, value in zip(signed_headers.split(';'), canonical_header_values)])
+        payload_hash = hashlib.sha256(body.encode('utf-8')).hexdigest()
+        canonical_querystring = ''
 
-        canonical_querystring = urllib.parse.urlencode(query)
+        if req.params:
+            canonical_querystring = urllib.parse.urlencode(sorted(req.params.items(), key=lambda x: (x[0], x[1])))
 
-        canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
-        credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
+        canonical_request = '\n'.join([req.method, canonical_uri, canonical_querystring, canonical_headers, signed_headers, payload_hash])
+        credential_scope = os.path.join(datestamp, region, service, 'aws4_request')
         signing_key = self.get_signature_key(self.aws_secret_key, datestamp, region, service)
-        string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
+        string_to_sign = '\n'.join([algorithm, amz_date, credential_scope, hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()])
         
         signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
-        authorization_header = algorithm + ' ' + 'Credential=' + self.aws_access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
-        headers = {
-            'host': host,
-            'user-agent': 'My SPAPI Client tool /1.0(Language=python/3.10',
-            'x-amz-access-token': access_token,
-            'x-amz-date': amz_date,
-            'Authorization': authorization_header,
-        }
-        return headers
+        authorization_header = f'{algorithm} Credential={self.aws_access_key}/{credential_scope}, SignedHeaders={signed_headers}, Signature={signature}'
+        
+        req.headers['x-amz-date'] = amz_date
+        req.headers['Authorization'] = authorization_header
+
+        return req
 
     def get_my_fees_estimate_for_asin(self, asin: str, price: int, is_fba: bool = True, currency_code: str = 'JPY'):
         logger.info('action=get_my_fees_estimate_for_asin status=run')
 
         method = 'POST'
         path = f'/products/fees/v0/items/{asin}/feesEstimate'
+        url = urllib.parse.urljoin(ENDPOINT, path)
         body =  {
             'FeesEstimateRequest': {
                 'Identifier': asin,
@@ -128,14 +138,33 @@ class SPAPI:
                 'MarketplaceId': self.marketplace_id, 
             }
         }
-        # query = {
-        #     'Asins': asin,
-        #     'ItemType': 'Asin',
-        #     'MarketplaceId': settings.MARKETPLACEID,
+        headers = {
+            'host': urllib.parse.urlparse(ENDPOINT).netloc,
+            'user-agent': 'My SPAPI Client tool /1.0(Language=python/3.10)',
+            'x-amz-access-token': self.get_spapi_access_token(),
+        }
+        req = requests.Request(method=method, url=url, json=body, headers=headers)
+        req = self.create_authorization_headers(req)
+        response = request(req)
 
-        # }
-        access_token = self.get_spapi_access_token()
+        return response
+
+    def get_pricing(self, asin_list: list, item_type: str='Asin'):
+        method = 'GET'
+        path = '/products/pricing/v0/price'
         url = urllib.parse.urljoin(ENDPOINT, path)
-        headers = self.create_signature(method, canonical_uri=path, access_token=access_token, body=body)
-        response = requests.post(url, json=body, headers=headers)
-        print(response.text)
+        query = {
+            'Asins': ','.join(asin_list),
+            'ItemType': item_type,
+            'MarketplaceId': self.marketplace_id,
+        }
+        headers = {
+            'host': urllib.parse.urlparse(ENDPOINT).netloc,
+            'user-agent': 'My SPAPI Client tool /1.0(Language=python/3.10)',
+            'x-amz-access-token': self.get_spapi_access_token(),
+        }
+        req = requests.Request(method=method, url=url, params=query, headers=headers)
+        req = self.create_authorization_headers(req)
+        response = request(req)
+
+        return response
