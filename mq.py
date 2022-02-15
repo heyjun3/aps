6e7669cffd57cd456210c7a018556ad839fda5c7
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-02-15 23:53:47 +0900

    fix mq

--- mq.py
+++ mq.py
@@ -1,4 +1,4 @@
-
+import json
 import pika
 
 
@@ -8,18 +8,22 @@ channel.queue_declare(queue='hello')
 
 
 def send():
-    channel.basic_publish(exchange='', routing_key='hello', body='hello World')
+    channel.basic_publish(exchange='', routing_key='hello', body=json.dumps({'hello': 1}))
     print('send')
 
 
 def receive():
 
-    def callback(ch, method, properties, body):
-        print(f'Received {body}')
-
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-    channel.start_consuming()
+    # for method_frame, properties, body in channel.consume('hello', auto_ack=True):
+    #     print(body)
+    # while True:
+    #     value = channel.consume('hello', auto_ack=True)
+    #     print(value)
+    # value = [channel.basic_get('hello', auto_ack=True) for _ in range(5) if channel.basic_get('hello', auto_ack=True)[2] is not None]
+    method_frame, header, body = channel.basic_get('hello', auto_ack=True)
+    print(body)
 
 
 if __name__ == '__main__':
-    send()
+    receive()
+    # send()
