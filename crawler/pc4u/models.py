22230075c813c6466e5fdc6039191f968e7f81a1
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-02-21 18:55:42 +0900

    fix mq pc4u

--- crawler/pc4u/models.py
+++ crawler/pc4u/models.py
@@ -1,11 +1,15 @@
+from sqlalchemy import Column
+from sqlalchemy import String
+
 from crawler.models import Product
 from crawler.models import Base
 from crawler.models import postgresql_engine
 
 
-class Pc4u(Product, Base):
+class Pc4uProduct(Product, Base):
     __tablename__ = 'pc4u_products'
 
+    shop_code = Column(String, primary_key=True, nullable=False, default='pc4u')
     
 
 def init_db():
