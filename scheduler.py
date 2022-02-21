8ccf7c00dba53e51d4c379a57e9c985fee9bce3c
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-02-22 02:03:28 +0900

    fix scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -5,8 +5,8 @@ import schedule
 
 from crawler.pc4u import pc4u
 from crawler.buffalo import buffalo
-from crawler.super import super
-from crawler.netsea import netsea
+from crawler.super import super_tasks
+from crawler.netsea import netsea_tasks
 from ims import repeat
 
 
@@ -16,9 +16,9 @@ def run_threaded(func):
 
 
 def register():
-    schedule.every().day.at('09:00').do(run_threaded, netsea.new_product_search)
-    schedule.every().day.at('05:00').do(run_threaded, super.schedule_super_task)
-    schedule.every().day.at('17:00').do(run_threaded, pc4u.schedule_pc4u_task_everyday)
+    schedule.every().day.at('09:00').do(run_threaded, netsea_tasks.run_new_product_search)
+    schedule.every().day.at('05:00').do(run_threaded, super_tasks.run_schedule_super_task)
+    schedule.every().day.at('17:00').do(run_threaded, pc4u.main)
     schedule.every().day.at('17:30').do(run_threaded, buffalo.main)
     # schedule.every().monday.at('01:00').do(run_threaded, netsea.new_shop_search)
     schedule.every().saturday.at('07:00').do(run_threaded, repeat.main)
