fbb82bcaeb1983e11a387c6981731f3e226b50e7
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-07-20 01:23:08 +0900

    fix: async func

--- mws/models.py
+++ mws/models.py
@@ -4,7 +4,6 @@ import threading
 from copy import deepcopy
 import itertools
 from typing import List
-import asyncio
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column
@@ -24,6 +23,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 
+from models_base import ModelsBase
 from keepa.models import KeepaProducts
 import settings
 import log_settings
@@ -46,7 +46,7 @@ PROFIT = "price - (cost * unit) - ((price * fee_rate) * 1.1) - shipping_fee"
 PROFIT_RATE = "(price - (cost * unit) - ((price * fee_rate) * 1.1) - shipping_fee) / price"
 
 
-class MWS(Base):
+class MWS(Base, ModelsBase):
     __tablename__ = 'mws_products'
     asin = Column(String, primary_key=True, nullable=False)
     filename = Column(String, primary_key=True, nullable=False)
@@ -61,107 +61,164 @@ class MWS(Base):
     profit_rate =Column(Numeric(precision=10, scale=2), Computed(PROFIT_RATE))
     created_at = Column(DateTime, default=datetime.datetime.now)
 
-    def save(self):
-        with session_scope() as session:
-            try:
-                session.add(self)
-            except IntegrityError as ex:
-                logger.debug(ex)
-                return False
-            return True
+    # def save(self):
+    #     with session_scope() as session:
+    #         try:
+    #             session.add(self)
+    #         except IntegrityError as ex:
+    #             logger.debug(ex)
+    #             return False
+    #         return True
+
+    async def save(self):
+        async with self.async_session as session:
+            session.add(self)
+        return True
+
+    # @classmethod
+    # def get_filenames(cls):
+    #     with session_scope() as session:
+    #         filenames = session.query(distinct(cls.filename)).all()
+    #         filenames = itertools.chain.from_iterable(filenames)
+
+    #     return sorted(filenames)
 
     @classmethod
-    def get_filenames(cls):
-        with session_scope() as session:
-            filenames = session.query(distinct(cls.filename)).all()
-            filenames = itertools.chain.from_iterable(filenames)
+    async def get_filenames(cls):
+        async with cls.async_session() as session:
+            stmt = select(cls).where(distinct(cls.filename))
+            result = await session.execute(stmt)
+            filenames = result.scalars().all()
+            return sorted(itertools.chain.from_iterable(filenames))
 
-        return sorted(filenames)
+    # @classmethod
+    # def get_chart_data(cls, filename: str, page: int, count: int) -> List: 
+    #     start = (page - 1) * count
+    #     end = start + count
+    #     with session_scope() as session:
+    #         rows = session.query(cls, KeepaProducts.render_data).join(KeepaProducts, cls.asin == KeepaProducts.asin)\
+    #                 .filter(cls.profit >= 200, 
+    #                         cls.profit_rate >= 0.1, 
+    #                         cls.filename == filename, 
+    #                         cls.unit <= 10, 
+    #                         KeepaProducts.sales_drops_90 > 3, 
+    #                         KeepaProducts.render_data != None)\
+    #                 .order_by(MWS.profit.desc()).slice(start, end).all()
+    #     return rows
 
     @classmethod
-    def get_done_filenames(cls):
-        with session_scope() as session:
-            filenames = session.query(distinct(cls.filename)).all()
-        filenames = itertools.chain.from_iterable(filenames) 
-
-        with session_scope() as session:
-            not_done_filenames = session.query(distinct(cls.filename)).join(KeepaProducts, cls.asin == KeepaProducts.asin, isouter=True)\
-                                 .filter(cls.profit >= 200, cls.profit_rate >= 0.1, KeepaProducts.asin == None).all()
-        not_done_filenames = itertools.chain.from_iterable(not_done_filenames)
-
-        return sorted(list(set(filenames) - set(not_done_filenames)))
-
-    @classmethod
-    def get_render_data(cls, filename: str, page: int=1, count: int=500):
+    async def get_chart_data(cls, filename: str, page: int, count: int) -> List:
         start = (page - 1) * count
         end = start + count
-        with session_scope() as session:
-            rows = session.query(cls, KeepaProducts.render_data).join(KeepaProducts, cls.asin == KeepaProducts.asin)\
-            .filter(cls.profit >= 200, cls.profit_rate >= 0.1, cls.filename == filename, 
-            cls.unit <= 10, KeepaProducts.sales_drops_90 > 3, KeepaProducts.render_data != None)\
-            .order_by(KeepaProducts.sales_drops_90.desc()).slice(start, end).all()
-            return rows
+        async with cls.async_session() as session:
+            stmt = select(cls, KeepaProducts.render_data).join(KeepaProducts, cls.asin == KeepaProducts.asin).where(
+                cls.profit >= 200,
+                cls.profit_rate >= 0.1,
+                cls.filename == filename,
+                cls.unit <= 10,
+                KeepaProducts.sales_drops_90 > 3,
+                KeepaProducts.render_data != None).order_by(cls.profit.desc()).slice(start, end).all()
+            result = await session.execute(stmt)
+            return result.scalars().all()
+
+    # @classmethod
+    # def get_row_count(cls, filename: str) -> int:
+    #     with session_scope() as session:
+    #         rows = session.query(func.count(cls.asin)).join(KeepaProducts, cls.asin == KeepaProducts.asin)\
+    #         .filter(cls.profit >= 200, cls.profit_rate >= 0.1, cls.filename == filename, cls.unit <= 10, KeepaProducts.sales_drops_90 > 3, KeepaProducts.render_data != None).first()
+    #         return rows[0]
 
     @classmethod
-    def get_chart_data(cls, filename: str, page: int, count: int) -> List: 
-        start = (page - 1) * count
-        end = start + count
-        with session_scope() as session:
-            rows = session.query(cls, KeepaProducts.render_data).join(KeepaProducts, cls.asin == KeepaProducts.asin)\
-                    .filter(cls.profit >= 200, 
-                            cls.profit_rate >= 0.1, 
-                            cls.filename == filename, 
-                            cls.unit <= 10, 
-                            KeepaProducts.sales_drops_90 > 3, 
-                            KeepaProducts.render_data != None)\
-                    .order_by(MWS.profit.desc()).slice(start, end).all()
-        return rows
+    async def get_row_count(cls, filename: str) -> int:
+        async with cls.async_session() as session:
+            stmt = select(func.count(cls.asin)).join(KeepaProducts, cls.asin == KeepaProducts.asin).where(
+                cls.profit >= 200,
+                cls.profit_rate >= 0.1,
+                cls.filename == filename,
+                cls.unit <= 10,
+                KeepaProducts.sales_drops_90 > 3,
+                KeepaProducts.render_data != None,
+            )
+            result = await session.execute(stmt)
+            return result.scalar().first()
+
+    # @classmethod
+    # def get_asin_list_None_products(cls, profit: int=200, profit_rate: float=0.1):
+    #     with session_scope() as session:
+    #         try:
+    #             asin_list = session.query(cls.asin).filter(cls.profit >= profit, cls.profit_rate >= profit_rate)\
+    #                         .join(KeepaProducts, cls.asin == KeepaProducts.asin, isouter=True).filter(KeepaProducts.asin == None).all()
+    #             asin_list = list(set(map(lambda x: x[0], asin_list)))
+    #         except Exception as ex:
+    #             logger.error(f'action=get_asin_to_request_keepa error={ex}')
+    #             return None
+    #         return asin_list
 
     @classmethod
-    def get_row_count(cls, filename: str) -> int:
-        with session_scope() as session:
-            rows = session.query(func.count(cls.asin)).join(KeepaProducts, cls.asin == KeepaProducts.asin)\
-            .filter(cls.profit >= 200, cls.profit_rate >= 0.1, cls.filename == filename, cls.unit <= 10, KeepaProducts.sales_drops_90 > 3, KeepaProducts.render_data != None).first()
-            return rows[0]
+    async def get_asin_list_None_products(cls, profit: int=200, profit_rate: float=0.1) -> List:
+        async with cls.async_session() as session:
+            stmt = select(cls.asin).join(KeepaProducts, cls.asin == KeepaProducts.asin, isouter=True).where(
+                cls.profit >= profit,
+                cls.profit_rate >= profit_rate,
+                KeepaProducts.asin == None)
+            result = await session.execute(stmt)
+            return itertools.chain.from_iterable(result.scalars().all())
+
+    # @classmethod
+    # def get_price_is_None_products(cls):
+    #     with session_scope() as session:
+    #         products = session.query(cls.asin).filter(cls.price == None).all()
+    #         return products
 
     @classmethod
-    def get_asin_list_None_products(cls, profit: int=200, profit_rate: float=0.1):
-        with session_scope() as session:
-            try:
-                asin_list = session.query(cls.asin).filter(cls.profit >= profit, cls.profit_rate >= profit_rate)\
-                            .join(KeepaProducts, cls.asin == KeepaProducts.asin, isouter=True).filter(KeepaProducts.asin == None).all()
-                asin_list = list(set(map(lambda x: x[0], asin_list)))
-            except Exception as ex:
-                logger.error(f'action=get_asin_to_request_keepa error={ex}')
-                return None
-            return asin_list
+    async def get_price_is_None_products(cls):
+        async with cls.async_session() as session:
+            stmt = select(cls.asin).where(cls.price == None)
+            result = await session.execute(stmt)
+            return result.scalars().all()
+
+    # @classmethod
+    # def get_price_is_None_asins(cls):
+    #     with session_scope() as session:
+    #         products = session.query(cls.asin).filter(cls.price == None).all()
+    #         products = list(map(lambda x: x[0], products))
+    #         return products
 
     @classmethod
-    def get_price_is_None_products(cls):
-        with session_scope() as session:
-            products = session.query(cls.asin).filter(cls.price == None).all()
-            return products
+    async def get_price_is_None_asins(cls):
+        async with cls.async_session() as session:
+            stmt = select(cls.asin).where(cls.price == None)
+            result = await session.execute(stmt)
+            return result.scalars().all()
+
+    # @classmethod
+    # def get_fee_is_None_products(cls):
+    #     with session_scope() as session:
+    #         products = session.query(cls.asin).filter(or_(cls.fee_rate == None, cls.shipping_fee == None)).all()
+    #         return products
 
     @classmethod
-    def get_price_is_None_asins(cls):
-        with session_scope() as session:
-            products = session.query(cls.asin).filter(cls.price == None).all()
-            products = list(map(lambda x: x[0], products))
-            return products
+    async def get_fee_is_None_products(cls):
+        async with cls.async_session() as session:
+            stmt = select(cls.asin).where(or_(cls.fee_rate == None, cls.shipping_fee == None))
+            result = await session.execute(stmt)
+            return result.scalars().all()
+
+    # @classmethod
+    # def get_fee_is_None_asins(cls, limit_count: int=1000):
+    #     with session_scope() as session:
+    #         products = session.query(cls.asin).filter(or_(cls.fee_rate == None, cls.shipping_fee == None))\
+    #         .order_by(cls.created_at).limit(limit_count).all()
+    #         products = list(map(lambda x: x[0], products))
+    #         return products
 
     @classmethod
-    def get_fee_is_None_products(cls):
-        with session_scope() as session:
-            products = session.query(cls.asin).filter(or_(cls.fee_rate == None, cls.shipping_fee == None)).all()
-            return products
-
-    @classmethod
-    def get_fee_is_None_asins(cls, limit_count: int=1000):
-        with session_scope() as session:
-            products = session.query(cls.asin).filter(or_(cls.fee_rate == None, cls.shipping_fee == None))\
-            .order_by(cls.created_at).limit(limit_count).all()
-            products = list(map(lambda x: x[0], products))
-            return products
+    async def get_fee_is_None_asins(cls, limit_count: int=1000):
+        async with cls.async_session() as session:
+            stmt = select(cls.asin).filter(or_(cls.fee_rate == None, cls.shipping_fee == None)
+                    .order_by(cls.created_at).limit(limit_count))
+            result = await session.execute(stmt)
+            return result.scalars().all()
 
     @classmethod
     def update_price(cls, asin: str, price: int):
@@ -248,13 +305,3 @@ def session_scope():
 
 def init_db(engine):
     Base.metadata.create_all(bind=engine)
-
-
-async def async_main():
-    async with async_session() as session:
-        stmt = select(MWS).where(MWS.asin=='B000FQRD6S').limit(1)
-        result = await session.execute(stmt)
-        product = result.scalars().first()
-        print(product.value)
-        product.unit = 3
-        await session.commit()
