36aaf0ac7c88e9c170014257d61424b1f6a661d9
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-09-22 12:19:44 +0900

    fix: log settings

--- log_settings.py
+++ log_settings.py
@@ -1,24 +1,30 @@
 import logging
-import os
 
-import settings
+from fluent import handler
 
 
 def get_logger(name: str, level=logging.INFO) -> logging.getLogger:
     logger = logging.getLogger(name)
     logger.setLevel(level)
     formatter = logging.Formatter('[%(asctime)-15s]%(levelname)s:%(name)s:%(message)s')
-
-    filehander = logging.FileHandler(os.path.join(settings.BASE_PATH, 'logs', f'{name}.log'))
-    filehander.setFormatter(formatter)
-    filehander.setLevel(level)
+    
+    fluent_handler = handler.FluentHandler('app', 'localhost', 9880)
+    fluent_format = {
+        'host': '%(hostname)s',
+        'time': '%(asctime)-15s',
+        'level': '%(levelname)s',
+        'name': '%(name)s',
+        'message': '%(message)s',
+    }
+    fluent_handler.setFormatter(handler.FluentRecordFormatter(fluent_format))
+    fluent_handler.setLevel(level)
 
     streamhandler = logging.StreamHandler()
     streamhandler.setFormatter(formatter)
     streamhandler.setLevel(level)
 
-    logger.addHandler(filehander)
     logger.addHandler(streamhandler)
+    logger.addHandler(fluent_handler)
 
     return logger
     
