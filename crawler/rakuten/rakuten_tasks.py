084ec8489604d44e4703239b257e60532465acec
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-10-24 23:37:25 +0900

    fix rakuten

--- crawler/rakuten/rakuten_tasks.py
+++ crawler/rakuten/rakuten_tasks.py
@@ -6,7 +6,8 @@ logger = log_settings.get_logger(__name__)
 
 
 SHOP_CODES = [
-    'superdeal', 'ksdenki', 'dj', 'e-zoa', 'reckb', 'jtus', 'ioplaza', 'ikebe',
+    # 'superdeal', 'ksdenki', 'dj', 'e-zoa', 'reckb', 
+    'ioplaza', 'ikebe',
     'shoptsukumo', 'premiumgt', 'acer-direct', 'wakeari', 'jism', 'sakurayama',
     'ikeshibu', 'aikyoku-bargain-center', 'aikyoku', 'ikebe-rockhouse', 'ishibashi',
     'pckoubou', 'e-earphone', 'applied2', 'yamada-denki', 'key', 'r-kojima']
@@ -25,7 +26,6 @@ def run_rakuten_search_all() -> None:
     logger.info('action=run_rakuten_search_all status=run')
 
     for shop_code in SHOP_CODES:
-        # run_rakuten_search_at_shop_code(shop_code)
         RakutenCrawler(shop_code).main()
 
     logger.info('action=run_rakuten_search_all status=done')
