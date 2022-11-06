d4879a51f1dcb83b0e27fe6866e1796338d93254
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-11-07 07:15:47 +0900

    fix rakuten crawler error

--- crawler/rakuten/rakuten.py
+++ crawler/rakuten/rakuten.py
@@ -191,6 +191,9 @@ class RakutenCrawler(object):
             ], searched_products + [product for product in products if product.get('jan')]) 
             if value]
 
+        if not parsed_value:
+            return
+
         next_query = self._generate_next_page_query(response, parsed_value[-1])
         logger.info({'next page query': next_query})
         return next_query
