79bd76d4c79bbfb095ab766e3fef6e2052b476f7
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-01-08 02:32:13 +0900

    get db object

--- services/migrate_timescaledb/main.go
+++ services/migrate_timescaledb/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"context"
-	"strconv"
-	"migrate_timescaledb/app/models"
+	"encoding/json"
+	"fmt"
 	"migrate_timescaledb/app/connection"
+	"migrate_timescaledb/app/models"
+
 	_ "github.com/lib/pq"
 )
 
+type detailData struct {
+	Date string `json:"date"`
+	Rank float64 `json:"rank"`
+	Price float64 `json:"price"`
+}
+
+type RenderData struct {
+	Data []detailData `json:"data"`
+}
+
+type Data struct {
+	Price map[string]float64
+}
+
 func main() {
-	count, err := models.AsinsInfos().Count(context.Background(), connection.DbConnection)
+	asin := "B07MTRXVR7"
+	product, err := models.FindKeepaProduct(context.Background(), connection.DbConnection, asin)
 	if err != nil {
-		fmt.Printf("Not get count %v", err)
+		fmt.Println("error")
 		return
 	}
-	fmt.Println(fmt.Sprintf("Count: %s", strconv.FormatInt(count, 10)))
+	fmt.Println(product.Asin)
+	fmt.Println(product.SalesDrops90)
+	fmt.Println(product.Created)
+	fmt.Println(product.Modified)
+	d := make(map[string]float64)
+	err = json.Unmarshal(product.PriceData.JSON, &d)
+	for k, v := range d {
+		fmt.Println(k, v)
+	}
 }
