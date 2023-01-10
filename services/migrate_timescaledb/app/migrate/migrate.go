aec1040a9bbf8c335c87416a6a6fceeb134a3375
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-01-11 00:36:57 +0900

    update

--- services/migrate_timescaledb/app/migrate/migrate.go
+++ services/migrate_timescaledb/app/migrate/migrate.go
@@ -1,12 +1,17 @@
 package migrate
 
 import (
+	"encoding/json"
+	"context"
 	"fmt"
 	"time"
 	"strconv"
+
+	con "migrate_timescaledb/app/connection"
+	"migrate_timescaledb/app/models"
 )
 
-func ConvKeepaTimeToTime(keepa_time string) (*time.Time, error) {
+func convKeepaTimeToTime(keepa_time string) (*time.Time, error) {
 	k, err := strconv.Atoi(keepa_time)
 	if err != nil {
 		fmt.Printf("keepa time arg is valid %v", keepa_time)
@@ -15,4 +20,24 @@ func ConvKeepaTimeToTime(keepa_time string) (*time.Time, error) {
 	unix_time := (k + 21564000) * 60
 	t := time.Unix(int64(unix_time), 0)
 	return &t, nil
+}
+
+func ConvKeepaProductToAsinsInfo(asin string) {
+	p, err := models.FindKeepaProduct(context.Background(), con.DbConnection, asin)
+	if err != nil {
+		fmt.Printf("get keepa product failed. argument is %v\n", asin)
+		return
+	}
+
+	prices := make(map[string]float64)
+	if err := json.Unmarshal(p.PriceData.JSON, &prices); err != nil {
+		fmt.Println("price data unmarshal error")
+		return
+	}
+
+	ranks := make(map[string]float64)
+	if err := json.Unmarshal(p.RankData.JSON, &ranks); err != nil {
+		fmt.Println("rank data unmarshal error")
+		return
+	}
 }
--- services/migrate_timescaledb/app/migrate/migrate_test.go
+++ services/migrate_timescaledb/app/migrate/migrate_test.go
@@ -11,14 +11,14 @@ import (
 
 func TestConvKeepaTimeToTime(t *testing.T) {
 	t.Run("convert keepa time to time.Time", func(t *testing.T) {
-		dt, err := ConvKeepaTimeToTime("5831784")
+		dt, err := convKeepaTimeToTime("5831784")
 
 		assert.Equal(t, nil, err)
 		assert.Equal(t, time.Date(2022, 2, 2, 5, 24, 0, 0, time.Local), *dt)
 	})
 
 	t.Run("a-z argument is not valid", func(t *testing.T) {
-		dt, err := ConvKeepaTimeToTime("aazzz")
+		dt, err := convKeepaTimeToTime("aazzz")
 		
 		assert.IsType(t, &strconv.NumError{}, err)
 		assert.Equal(t, time.Time{}, *dt)
