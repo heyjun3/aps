3a5262c1489b37cd620a71508dab831192af30b1
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-01-22 20:53:53 +0900

    add dashboard

--- services/front/src/components/Dashboard.js
+++ services/front/src/components/Dashboard.js
@@ -1,9 +1,26 @@
 import React from "react"
-import OutlinedCard from "./OutlinedCard"
+
+import config from "../config"
+// import OutlinedCard from "./OutlinedCard"
 
 const Dashboard = () => {
+    const [keepa, setKeepa] = React.useState({})
+    const [mws, setMws] = React.useState({})
+
+    React.useEffect(() => {
+        fetch(`${config.fqdn}/api/counts`, {method: "GET", mode: "cors"})
+        .then(res => res.json())
+        .then(data => {
+            setKeepa(data.keepa)
+            setMws(data.mws)
+        })
+    }, [])
     return (
-        <OutlinedCard/>
+        <div>
+            {keepa ? <h3>Keepa Table modified Count: {keepa.modified} / {keepa.total}</h3> : ""}
+            {mws ? <h3>MWS Table price Count: {mws.price} / {mws.total}</h3> : ""}
+            {mws ? <h3>MWS Table fee Count: {mws.fee} / {mws.total}</h3> : ""}
+        </div>
     )
 }
 
