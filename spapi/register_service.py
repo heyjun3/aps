34c335badcee384780b7226e2db5a7c8c42d368a
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-02-04 10:16:32 +0900

    fix spapi service

--- spapi/register_service.py
+++ spapi/register_service.py
@@ -65,8 +65,11 @@ class RegisterService(object):
         db = self.client.open(title).worksheet(keep_sheet)
         db.append_rows(rows)
 
-        for row in rows:
-            cell = add.find(row.get("SKU"), in_column=5)
+        for record in records:
+            sku = record.get("SKU")
+            if sku is None:
+                continue
+            cell = add.find(sku, in_column=5)
             time.sleep(1)
             if cell:
                 add.delete_row(cell.row)
