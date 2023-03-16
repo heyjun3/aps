fe1a9015f3e69b4b1a1186b9a3ec6c249c617fc3
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-03-17 01:18:24 +0900

    fix

--- services/crawler/ikebe/models.go
+++ services/crawler/ikebe/models.go
@@ -11,6 +11,30 @@ import (
 	"crawler/config"
 )
 
+type IkebeItem struct {
+	bun.BaseModel `bun:"table:ikebe_product"`
+	Name string
+	Jan string
+	Price int64
+	ShopCode string `bun:"shop_code,pk"`
+	ProductCode string `bun:"product_code,pk"`
+	URL string
+}
+
+func (i *IkebeItem) Upsert(conn *bun.DB, ctx context.Context) error {
+	_, err := conn.NewInsert().
+		Model(i).
+		On("CONFLICT (shop_code, product_code) DO UPDATE").
+		Set(`
+		name = EXCLUDED.name,
+		jan = EXCLUDED.jan,
+		price = EXCLUDED.price,
+		url = EXCLUDED.url
+		`).
+		Exec(ctx)
+	return err
+}
+
 type User struct {
 	bun.BaseModel `bun:"table:users,alias:u"`
 
@@ -21,13 +45,17 @@ type User struct {
 func Tmp() {
 	conn := createDBConnection(config.DBDsn)
 	ctx := context.Background()
-	u := &User{
-		Name: "test",
+	i := &IkebeItem{
+		ShopCode: "test",
+		ProductCode: "test",
+		Name: "name2",
 	}
-	err := u.Upsert(conn, ctx)
+	err := i.Upsert(conn, ctx)
 	if err != nil {
 		logger.Error("error", err)
 	}
+	Test(test1)
+	Test(test2("a", "aa"))
 }
 
 func createDBConnection(dsn string) *bun.DB{
@@ -54,7 +82,23 @@ func (u *User) Upsert(conn *bun.DB, ctx context.Context) error {
 	_, err := conn.NewInsert().
 		Model(u).
 		On("CONFLICT (id) DO UPDATE").
-		Set("? = EXCLUDED.?", bun.Ident("name"), "name").
+		Set("? = ?", bun.Ident("name"), "name").
 		Exec(ctx)
 	return err
+}
+
+func test1(s string) {
+	logger.Info(s)
+}
+
+func test2(s1, s2 string) func(string){
+	return func(s string) {
+		logger.Info(s1)
+		logger.Info(s2)
+		logger.Info(s)
+	}
+}
+
+func Test(f func(string)) {
+	f("aaaa")
 }
