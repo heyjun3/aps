07d011b78e3b024283623911b7cde9e67948b10b
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-04-22 16:15:16 +0900

    set user agent

--- services/crawler/config/config.go
+++ services/crawler/config/config.go
@@ -36,6 +36,7 @@ var DBDsn string
 var MQDsn string
 var DstMQDsn string
 var Logger *slog.Logger
+var UserAgent = "ozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
 func init() {
 	Logger = slog.New(slog.NewJSONHandler(os.Stdout))
