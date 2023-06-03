5bbc80a7c3247b21482aedcd4d3b7c0955781090
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-06-03 14:41:30 +0900

    add integration tests

--- services/crawler/test/integration/parser_integration_test.go
+++ services/crawler/test/integration/parser_integration_test.go
@@ -1,3 +1,4 @@
+//go:build integration
 // +build integration
 
 package integration
@@ -7,48 +8,102 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"crawler/scrape"
+	"crawler/ark"
+	"crawler/ikebe"
+	"crawler/nojima"
+	"crawler/pc4u"
 	"crawler/rakuten"
+	"crawler/scrape"
 )
 
-func TestProductListIntegration(t *testing.T){
-	t.Parallel()
+func TestProductListIntegration(t *testing.T) {
 	type args struct {
-		url string
+		parser scrape.Parser
+		url    string
 	}
 	type want struct {
 		count int
-		url string
+		url   string
 	}
 
-	tests := []struct{
-		name string
-		args args
-		want want
-	}{{
-		name: "parse product list integration test",
-		args: args{
-			url: "https://search.rakuten.co.jp/search/mall/?sid=197844",
+	tests := []struct {
+		name   string
+		args   args
+		want   want
+		hasJan bool
+	}{
+		{
+			name: "ark product list",
+			args: args{
+				parser: ark.ArkParser{},
+				url:    "https://www.ark-pc.co.jp/search/?limit=50&nouki=1",
+			},
+			want: want{
+				count: 50,
+				url:   "https://www.ark-pc.co.jp/search/?offset=50&limit=50&nouki=1",
+			},
 		},
-		want: want{
-			count: 45,
-			url: "https://search.rakuten.co.jp/search/mall/?p=2&sid=197844",
+		{
+			name: "ikebe product list",
+			args: args{
+				parser: ikebe.IkebeParser{},
+				url:    "https://www.ikebe-gakki.com/Form/Product/ProductList.aspx?shop=0&cat=&bid=ec&dpcnt=40&img=1&sort=07&udns=1&fpfl=0&sfl=0&pno=1&cicon=1",
+			},
+			want: want{
+				count: 40,
+				url:   "https://www.ikebe-gakki.com/Form/Product/ProductList.aspx?shop=0&cat=&bid=ec&cicon=1&dpcnt=40&img=1&sort=07&udns=1&fpfl=0&sfl=0&pno=2",
+			},
 		},
-	}}
+		{
+			name: "nojima product list",
+			args: args{
+				parser: nojima.NojimaParser{},
+				url:    "https://online.nojima.co.jp/app/catalog/list/init?searchCategoryCode=0&mode=image&pageSize=60&currentPage=1&alignmentSequence=9&searchDispFlg=true&immediateDeliveryDispFlg=1&searchWord=+",
+			},
+			want: want{
+				count: 60,
+				url:   "https://online.nojima.co.jp/app/catalog/list/init?alignmentSequence=9&currentPage=2&immediateDeliveryDispFlg=1&mode=image&pageSize=60&searchCategoryCode=0&searchDispFlg=true&searchWord=+",
+			},
+			hasJan: true,
+		},
+		{
+			name: "pc4u product list",
+			args: args{
+				parser: pc4u.Pc4uParser{},
+				url:    "https://www.pc4u.co.jp/view/search?search_keyword=&search_price_low=&search_price_high=&search_category=&search_original_code=",
+			},
+			want: want{
+				count: 50,
+				url:   "https://www.pc4u.co.jp/view/search?page=2",
+			},
+		},
+		{
+			name: "rakuten product list",
+			args: args{
+				url:    "https://search.rakuten.co.jp/search/mall/?sid=197844",
+				parser: rakuten.RakutenParser{},
+			},
+			want: want{
+				count: 45,
+				url:   "https://search.rakuten.co.jp/search/mall/?p=2&sid=197844",
+			},
+		},
+	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := rakuten.RakutenParser{}
-			res, err := scrape.NewClient().Request("GET", tt.args.url, nil)
+		tc := tt
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			res, err := scrape.NewClient().Request("GET", tc.args.url, nil)
 			if err != nil {
 				panic(err)
 			}
 			defer res.Body.Close()
 
-			products, url := p.ProductList(res.Body, tt.args.url)
+			products, url := tc.args.parser.ProductList(res.Body, tc.args.url)
 
-			assert.Equal(t, tt.want.count, len(products))
-			assert.Equal(t, tt.want.url, url)
+			assert.Equal(t, tc.want.count, len(products))
+			assert.Equal(t, tc.want.url, url)
 
 			for _, p := range products {
 				assert.NotEmpty(t, p.GetName())
@@ -56,49 +111,96 @@ func TestProductListIntegration(t *testing.T){
 				assert.NotEmpty(t, p.GetProductCode())
 				assert.NotEmpty(t, p.GetShopCode())
 				assert.NotEmpty(t, p.GetURL())
+
+				if tc.hasJan {
+					assert.NotEmpty(t, p.GetJan())
+				}
 			}
 		})
 	}
 }
 
 func TestProductIntegration(t *testing.T) {
-	t.Parallel()
 	type args struct {
-		url string
+		url    string
+		parser scrape.Parser
 	}
 	type want struct {
 		jan string
 	}
 
-	tests := []struct{
-		name string
-		args args
-		want want
+	tests := []struct {
+		name    string
+		args    args
+		want    want
 		wantErr bool
-	}{{
-		name: "parse product",
-		args: args{
-			url: "https://item.rakuten.co.jp/dj/596022/?s-id=bk_pc_item_list_name_d",
+	}{
+		{
+			name: "ark product",
+			args: args{
+				url:    "https://www.ark-pc.co.jp/i/10170046/",
+				parser: ark.ArkParser{},
+			},
+			want: want{
+				jan: "0735858504447",
+			},
 		},
-		want: want{
-			jan: "4042477257071",
+		{
+			name: "ikebe product",
+			args: args{
+				url:    "https://www.ikebe-gakki.com/Form/Product/ProductDetail.aspx?shop=0&pid=758741&bid=ec&cat=dli003001",
+				parser: ikebe.IkebeParser{},
+			},
+			want: want{
+				jan: "4033653012218",
+			},
 		},
-		wantErr: false,
-	}}
+		{
+			name: "nojima product",
+			args: args{
+				url:    "https://online.nojima.co.jp/commodity/1/4902370550733/",
+				parser: nojima.NojimaParser{},
+			},
+			want: want{
+				jan: "4902370550733",
+			},
+		},
+		{
+			name: "pc4u product",
+			args: args{
+				url:    "https://www.pc4u.co.jp/view/item/000000082708",
+				parser: pc4u.Pc4uParser{},
+			},
+			want: want{
+				jan: "0840006698692",
+			},
+		},
+		{
+			name: "rakuten product",
+			args: args{
+				url:    "https://item.rakuten.co.jp/dj/596022/?s-id=bk_pc_item_list_name_d",
+				parser: rakuten.RakutenParser{},
+			},
+			want: want{
+				jan: "4042477257071",
+			},
+		},
+	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			p := rakuten.RakutenParser{}
-			res, err := scrape.NewClient().Request("GET", tt.args.url, nil)
+		tc := tt
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			res, err := scrape.NewClient().Request("GET", tc.args.url, nil)
 			if err != nil {
 				panic(err)
 			}
 			defer res.Body.Close()
 
-			jan, err := p.Product(res.Body)
+			jan, err := tc.args.parser.Product(res.Body)
 
-			assert.Equal(t, tt.want.jan, jan)
-			if tt.wantErr {
+			assert.Equal(t, tc.want.jan, jan)
+			if tc.wantErr {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
