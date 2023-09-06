e6d269f092b35d401a4276caa16f01c13b135bcf
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-09-07 02:44:15 +0900

    fix

--- services/crawler/bomber/parser.go
+++ services/crawler/bomber/parser.go
@@ -1,10 +1,14 @@
 package bomber
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 
 	"crawler/scrape"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type BomberParser struct{}
@@ -14,5 +18,16 @@ func (p BomberParser) ProductListByReq(r io.ReadCloser, req *http.Request) (scra
 }
 
 func (p BomberParser) Product(r io.ReadCloser) (string, error) {
-	return "", nil
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return "", err
+	}
+	re := regexp.MustCompile("[0-9]{12,13}")
+
+	codes := re.FindAllString(doc.Find(".detail_goods_name2").Text(), -1)
+	if len(codes) > 0 {
+		return codes[0], nil
+	}
+
+	return "", fmt.Errorf("not found jan code")
 }
--- services/crawler/bomber/service.go
+++ services/crawler/bomber/service.go
@@ -2,6 +2,11 @@ package bomber
 
 import (
 	"crawler/config"
+	"crawler/scrape"
 )
 
 var logger = config.Logger
+
+func NewScrapeService() scrape.Service[*BomberProduct] {
+	return scrape.NewService(BomberParser{}, &BomberProduct{}, []*BomberProduct{})
+}
