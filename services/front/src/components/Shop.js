44c25c54ac43c8e9f3a48bae3e6ccae683dc52fe
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-10-10 06:01:02 +0900

    update

--- services/front/src/components/Shop.js
+++ services/front/src/components/Shop.js
@@ -1,40 +1,30 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import config from '../config'
 
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "id", headerName: "ID", width: 130 },
+  { field: "site_name", headerName: "Site name", width: 130 },
+  { field: "name", headerName: "Shop name", width: 130 },
   {
-    field: "age",
-    headerName: "Age",
-    type: "number",
+    field: "interval",
+    headerName: "Interval",
     width: 90,
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    field: "url",
+    headerName: "URL",
+    width: 300,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxieaaaaaaaaaaaaaaaaaaa", firstName: "Harvey", age: 65 },
-];
-
 export const Shops = () => {
+  const [rows, setRows] = useState([])
+  useEffect(() => {
+    fetch(`${config.fqdn}/api/shops`, {method: "GET", mode: "cors"})
+    .then(res => res.json())
+    .then(res => setRows(res.shop))
+  }, [])
   return (
     <div style={{ width: "80%", margin: "auto", paddingTop: "50px" }}>
       <DataGrid
