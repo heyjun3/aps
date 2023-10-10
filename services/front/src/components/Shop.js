2da880ec5b6023b14d643372e2cee348d04dcd91
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-10-11 05:11:21 +0900

    fix

--- services/front/src/components/Shop.js
+++ services/front/src/components/Shop.js
@@ -1,32 +1,57 @@
 import { useEffect, useState } from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import config from '../config'
-
-const columns = [
-  { field: "id", headerName: "ID", width: 130 },
-  { field: "site_name", headerName: "Site name", width: 130 },
-  { field: "name", headerName: "Shop name", width: 130 },
-  {
-    field: "interval",
-    headerName: "Interval",
-    width: 90,
-  },
-  {
-    field: "url",
-    headerName: "URL",
-    width: 300,
-  },
-];
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import config from "../config";
 
 export const Shops = () => {
-  const [rows, setRows] = useState([])
+  const [rows, setRows] = useState([]);
+
+  const handleDeleteClick = (id) => () => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 130 },
+    { field: "site_name", headerName: "Site name", width: 130 },
+    { field: "name", headerName: "Shop name", width: 130 },
+    {
+      field: "interval",
+      headerName: "Interval",
+      width: 90,
+    },
+    {
+      field: "url",
+      headerName: "URL",
+      width: 300,
+      aggregable: false,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
   useEffect(() => {
-    fetch(`${config.fqdn}/api/shops`, {method: "GET", mode: "cors"})
-    .then(res => res.json())
-    .then(res => setRows(res.shop))
-  }, [])
+    fetch(`${config.fqdn}/api/shops`, { method: "GET", mode: "cors" })
+      .then((res) => res.json())
+      .then((res) => setRows(res.shop));
+  }, []);
+
   return (
-    <div style={{ width: "80%", margin: "auto", paddingTop: "50px" }}>
+    <div style={{ width: "90%", margin: "auto", paddingTop: "50px" }}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -36,7 +61,6 @@ export const Shops = () => {
           },
         }}
         pageSizeOptions={[5, 10]}
-        checkboxSelection
       />
     </div>
   );
