69ac74b74fce570076e42e77b84b4cea7fcffd37
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-10-12 03:13:41 +0900

    update

--- services/front/src/components/Shop.js
+++ services/front/src/components/Shop.js
@@ -1,7 +1,40 @@
 import { useEffect, useState } from "react";
-import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridToolbarContainer,
+} from "@mui/x-data-grid";
 import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import { v4 as uuidv4 } from "uuid";
 import config from "../config";
+
+const EditToolbar = (props) => {
+  const { setRows } = props;
+
+  const handleClick = () => {
+    setRows((oldRows) => [
+      {
+        randomId: uuidv4(),
+        id: "",
+        site_name: "",
+        name: "",
+        interval: "",
+        url: "",
+      },
+      ...oldRows,
+    ]);
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button coloer="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add record
+      </Button>
+    </GridToolbarContainer>
+  );
+};
 
 export const Shops = () => {
   const [rows, setRows] = useState([]);
@@ -11,19 +44,22 @@ export const Shops = () => {
   };
 
   const columns = [
-    { field: "id", headerName: "ID", width: 130 },
-    { field: "site_name", headerName: "Site name", width: 130 },
-    { field: "name", headerName: "Shop name", width: 130 },
+    { field: "id", headerName: "ID", width: 130, editable: true },
+    { field: "site_name", headerName: "Site name", width: 130, editable: true },
+    { field: "name", headerName: "Shop name", width: 130, editable: true },
     {
       field: "interval",
       headerName: "Interval",
       width: 90,
+      editable: true,
+      type: "singleSelect",
+      valueOptions: ["daily", "weekly"],
     },
     {
       field: "url",
       headerName: "URL",
       width: 300,
-      aggregable: false,
+      editable: true,
     },
     {
       field: "actions",
@@ -60,7 +96,15 @@ export const Shops = () => {
             paginationMode: { page: 0, pageSize: 5 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[5]}
+        slots={{
+          toolbar: EditToolbar,
+        }}
+        slotProps={{
+          toolbar: { setRows },
+        }}
+        disableRowSelectionOnClick={true}
+        editMode="row"
       />
     </div>
   );
