00a1bb7bcd669584e56cb526c2b96530fdde02d0
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-10-22 22:50:40 +0900

    fix

--- services/api-server/inventory/handler.go
+++ services/api-server/inventory/handler.go
@@ -1,6 +1,8 @@
 package inventory
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -11,27 +13,80 @@ import (
 	"golang.org/x/exp/slog"
 )
 
+var SpapiServiceURL string
+
+func init() {
+	SpapiServiceURL = os.Getenv("SPAPI_SERVICE_URL")
+	if SpapiServiceURL == "" {
+		panic(errors.New("don't set SPAPI_SERVICE_URL"))
+	}
+}
+
+type Pagination struct {
+	NextToken string `json:"nextToken"`
+}
+
+type Granularity struct {
+	GranularityType string `json:"granularityType"`
+	GranularityId   string `json:"granularityId"`
+}
+
+type Inventory struct {
+	Asin            string `json:"asin"`
+	FnSku           string `json:"fnSku"`
+	SellerSku       string `json:"sellerSku"`
+	Condition       string `json:"condition"`
+	LastUpdatedTime string `json:"lastUpdatedTime"`
+	ProductName     string `json:"productName"`
+	TotalQuantity   int    `json:"totalQuantity"`
+}
+
+type Payload struct {
+	Granularity        Granularity `json:"granularity"`
+	InventorySummaries []Inventory `json:"inventorySummaries"`
+}
+
+type InventorySummariesResponse struct {
+	Pagination Pagination `json:"pagination"`
+	Payload    Payload    `json:"payload"`
+}
+
 func RefreshInventory(c echo.Context) error {
-	host := os.Getenv("SPAPI_HOST")
-	if host == "" {
-		slog.Error("No set SPAPI_HOST")
-		return c.JSON(http.StatusInternalServerError, "internal server error")
-	}
-	query := url.Values{}
-	query.Set("next_token", "")
-	url := url.URL{
-		Scheme: "http",
-		Host: fmt.Sprintf("%s:8000", host),
-		Path: "inventory-summaries",
-		RawQuery: query.Encode(),
-	}
-	res, err := http.Get(url.String())
+	URL, err := url.Parse(SpapiServiceURL)
 	if err != nil {
-		slog.Error("get inventory summaries error", err)
-		return c.JSON(http.StatusInternalServerError, "internal server error")
+		return c.JSON(http.StatusInternalServerError, err)
 	}
-	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
-	fmt.Println(string(body))
+	URL.Path = "inventory-summaries"
+	var nextToken string
+	for {
+		res, err := func() (*InventorySummariesResponse, error) {
+			query := url.Values{}
+			query.Set("next_token", nextToken)
+			URL.RawQuery = query.Encode()
+			res, err := http.Get(URL.String())
+			if err != nil {
+				return nil, err
+			}
+			defer res.Body.Close()
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				return nil, err
+			}
+			var summariesResponse InventorySummariesResponse
+			if err := json.Unmarshal(body, &summariesResponse); err != nil {
+				return nil, err
+			}
+			nextToken = summariesResponse.Pagination.NextToken
+			return &summariesResponse, nil
+		}()
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err)
+		}
+		fmt.Println(res.Payload.InventorySummaries)
+		if nextToken == "" {
+			slog.Info("break loop")
+			break
+		}
+	}
 	return c.JSON(http.StatusOK, "success")
 }
