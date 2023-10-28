7c652b19e35db66a002ebc8d8d564d5ea9931e72
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-10-28 19:31:26 +0900

    fix

--- services/api-server/inventory/service.go
+++ services/api-server/inventory/service.go
@@ -2,6 +2,7 @@ package inventory
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/uptrace/bun"
+	"golang.org/x/exp/slog"
 )
 
 type InventoryService struct{}
@@ -41,8 +43,40 @@ func (i InventoryService) UpdateQuantity(ctx context.Context, db *bun.DB, invent
 	return inventoryRepository.Save(ctx, db, updateInventories)
 }
 
+type GetPricingResponse struct {
+	Paylaod []GetPricingPayload `json:"payload"`
+}
+
+type GetPricingPayload struct {
+	Status    string            `json:"status"`
+	SellerSKU string            `json:"SellerSKU"`
+	Product   GetPricingProduct `json:"Product"`
+}
+
+type GetPricingProduct struct {
+	Offers []Offers `json:"Offers"`
+}
+
+type Offers struct {
+	BuyingPrice BuyingPrice `json:"BuyingPrice"`
+}
+
+type BuyingPrice struct {
+	ListingPrice Price  `json:"ListingPrice"`
+	Points       Points `json:"Points"`
+}
+
+type Price struct {
+	CurrencyCode string  `json:"CurrencyCode"`
+	Amount       float64 `json:"Amount"`
+}
+
+type Points struct {
+	PointsNumber int64 `json:"PointsNumber"`
+}
+
 func (i InventoryService) UpdatePricing() error {
-	skus := []string{"4719512109847-N-1756-20231001"}
+	skus := []string{"4719512109847-N-1756-20231001", "4515260019373-B-2500-20231025"}
 	query := url.Values{}
 	query.Set("ids", strings.Join(skus, ","))
 	query.Set("id_type", "Sku")
@@ -62,6 +96,11 @@ func (i InventoryService) UpdatePricing() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(body))
+	var getPricingResponse GetPricingResponse
+	if err := json.Unmarshal(body, &getPricingResponse); err != nil {
+		slog.Error("err", err)
+		return err
+	}
+	fmt.Println(getPricingResponse.Paylaod[0].Product.Offers[0].BuyingPrice)
 	return nil
 }
