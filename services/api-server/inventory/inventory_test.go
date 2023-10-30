882bc1f458f383fc1e02753ee6fc1103ae42508f
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-10-30 22:30:46 +0900

    add tests

--- services/api-server/inventory/inventory_test.go
+++ services/api-server/inventory/inventory_test.go
@@ -3,6 +3,8 @@ package inventory
 import (
 	"api-server/test"
 	"context"
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -80,6 +82,69 @@ func TestGetBySellerSKU(t *testing.T) {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestGetNextPage(t *testing.T) {
+	db := test.CreateTestDBConnection()
+	test.ResetModel(context.Background(), db, &Inventory{})
+	repo := InventoryRepository{}
+	seed := make([]*Inventory, 100)
+	for i := range seed {
+		seed[i] = &Inventory{SellerSku: strconv.Itoa(i + 1), TotalQuantity: 10}
+	}
+	if err := repo.Save(context.Background(), db, seed); err != nil {
+		panic(err)
+	}
+
+	type args struct {
+		cursor string
+		limit  int
+	}
+	type want struct {
+		cursor Cursor
+		count  int
+		first  Inventory
+		last   Inventory
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    want
+		wantErr bool
+	}{
+		{
+			name:    "get next page",
+			args:    args{cursor: "10", limit: 20},
+			want:    want{cursor: Cursor{Start: "100", End: "28"}, first: Inventory{SellerSku: "100"}, last: Inventory{SellerSku: "28"}, count: 20},
+			wantErr: false,
+		},
+		{
+			name:    "no page",
+			args:    args{cursor: "998", limit: 100},
+			want:    want{count: 0, cursor: Cursor{}},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			inventories, cursor, err := repo.GetNextPage(context.Background(), db, tt.args.cursor, tt.args.limit)
+
+			fmt.Println(inventories, cursor, err)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want.count, len(inventories))
+				if tt.want.count > 0 {
+					assert.Equal(t, tt.want.first.SellerSku, inventories[0].SellerSku)
+					assert.Equal(t, tt.want.last.SellerSku, inventories[len(inventories)-1].SellerSku)
+				}
+				assert.Equal(t, tt.want.cursor, cursor)
 			}
 		})
 	}
