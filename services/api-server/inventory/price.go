9d6319fc01865a81381284c5f8036e958756f925
heyjun3 <t3k2g5kako@yahoo.co.jp>
2023-11-08 23:24:55 +0900

    fix

---
+++ services/api-server/inventory/price.go
@@ -0,0 +1,25 @@
+package inventory
+
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type Price struct {
+	SellerSku     string    `bun:"seller_sku,pk"`
+	Amount        int       `bun:"amount"`
+	Point         int       `bun:"point"`
+	CreatedAt     time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
+	UpdatedAt     time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
+}
+
+type CurrentPrice struct {
+	bun.BaseModel `bun:"table:prices"`
+	Price
+}
+
+type LowestPrice struct {
+	bun.BaseModel `bun:"table:lowest_prices"`
+	Price
+}
