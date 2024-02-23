f76b6d40621026b38f2b9b9f782e9bb4c6ca64e4
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-02-23 15:42:06 +0900

    fix

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-async function scrollBottom(count=100, intervalSec=2000) {
-    for(let i = 0; i < count; i++) {
-        window.scrollTo(0, document.documentElement.scrollHeight);
-        await new Promise((resolve, reject) => setTimeout(resolve, intervalSec))
-    }
+function sleep(intervalSec) {
+  return new Promise((resolve) => setTimeout(resolve, intervalSec));
+}
+
+async function scrollBottom(count = 100, intervalSec = 2000) {
+  for (let i = 0; i < count; i++) {
+    window.scrollTo(0, document.documentElement.scrollHeight);
+    await sleep(intervalSec);
+  }
 }
 
 async function openTab(url, intervalSec = 500) {
-    const tab = window.open(url, '_blank');
-    await new Promise((resolve, reject) => setTimeout(resolve, intervalSec))
-    tab.close()
-    await new Promise((resolve, reject) => setTimeout(resolve, intervalSec))
+  const tab = window.open(url, "_blank");
+  await sleep(intervalSec);
+  tab.close();
+  await sleep(intervalSec);
 }
 
 async function main() {
-    const elems = document.querySelectorAll('.link-area.needsclick.append-anchor')
-    for (const elem of elems) {
-        await openTab(elem.href)
-    }
+  await scrollBottom();
+  const elems = document.querySelectorAll(
+    ".link-area.needsclick.append-anchor"
+  );
+  for (const elem of elems) {
+    await openTab(elem.href);
+  }
 }
+
+main();
