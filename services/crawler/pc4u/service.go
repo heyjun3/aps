2ac77556da082ac54fe7c0948b049f169bf1a6fa
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-03-25 20:13:50 +0900

    fix cron settings

--- services/crawler/pc4u/service.go
+++ services/crawler/pc4u/service.go
@@ -1,12 +1,27 @@
 package pc4u
 
 import (
+	"strings"
+	"time"
+
 	"crawler/config"
 	"crawler/scrape"
 )
 
 var logger = config.Logger
 
-func NewScrapeService() scrape.Service[*Pc4uProduct] {
-	return scrape.NewService(Pc4uParser{}, &Pc4uProduct{}, []*Pc4uProduct{})
+func NewScrapeService(opts ...scrape.Option[*Pc4uProduct]) scrape.Service[*Pc4uProduct] {
+	return scrape.NewService(Pc4uParser{}, &Pc4uProduct{}, []*Pc4uProduct{}, opts...)
+}
+
+func ScrapeAll(shop string) {
+	urls := []string{
+		"https://www.pc4u.co.jp/view/category/outlet",
+		"https://www.pc4u.co.jp/view/search",
+	}
+	fileId := strings.Join([]string{shop, scrape.TimeToStr(time.Now())}, "_")
+	service := NewScrapeService(scrape.WithFileId[*Pc4uProduct](fileId))
+	for _, url := range urls {
+		service.StartScrape(url, shop)
+	}
 }
