5295a94d72b6b51c9f639a0776dc46e3124be544
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-04-01 19:48:02 +0900

    fix

--- services/crawler/schema.sql
+++ services/crawler/schema.sql
@@ -1,10 +1,13 @@
-CREATE TABLE products (
-    site_code VARCHAR NOT NULL,
-    shop_code VARCHAR NOT NULL,
-    product_code VARCHAR NOT NULL,
-    name VARCHAR,
-    jan VARCHAR,
-    price BIGINT,
-    url VARCHAR,
-    PRIMARY KEY(site_code, shop_code, product_code)
-)
+-- CREATE TABLE products (
+--     site_code VARCHAR NOT NULL,
+--     shop_code VARCHAR NOT NULL,
+--     product_code VARCHAR NOT NULL,
+--     name VARCHAR,
+--     jan VARCHAR,
+--     price BIGINT,
+--     url VARCHAR,
+--     PRIMARY KEY(site_code, shop_code, product_code)
+-- ) PARTITION BY LIST (site_code);
+
+-- CREATE TABLE products_default PARTITION OF products DEFAULT;
+-- CREATE TABLE products_ark PARTITION OF products FOR VALUES IN ('ark');
