55cd711a3757e87dc3e18972f0a7faa31f4cc380
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-04-08 20:24:46 +0900

    fix

--- services/crawler/ark/models.go
+++ services/crawler/ark/models.go
@@ -3,17 +3,30 @@ package ark
 import (
 	"github.com/uptrace/bun"
 
+	"crawler/product"
 	"crawler/scrape"
 )
 
-func NewArkProduct(name, productCode, url, jan string, price int64) (*ArkProduct, error) {
-	p, err := scrape.NewProduct(name, productCode, url, jan, "ark", price)
-	if err != nil {
-		return nil, err
-	}
-	return &ArkProduct{
-		Product: *p,
-	}, nil
+// func NewArkProduct(name, productCode, url, jan string, price int64) (*ArkProduct, error) {
+// 	p, err := scrape.NewProduct(name, productCode, url, jan, "ark", price)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return &ArkProduct{
+// 		Product: *p,
+// 	}, nil
+// }
+
+func NewArkProduct(name, productCode, url, jan string, price int64) (*product.Product, error) {
+	return product.New(product.Product{
+		SiteCode:    "ark",
+		ShopCode:    "ark",
+		ProductCode: productCode,
+		Name:        name,
+		URL:         url,
+		Price:       price,
+		Jan:         &jan,
+	})
 }
 
 type ArkProduct struct {
