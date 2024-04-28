b2a5e032822edb08959c8014f44466592a98b2ef
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-04-28 20:03:41 +0900

    fix sh

--- setup-kubernetes.sh
+++ setup-kubernetes.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+lsmod | grep br_netfilter
+lsmod | grep overlay
+
 # kernel param and swapp off
 sudo swapoff -a
 sudo sh -c "cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 vm.swappiness = 0
 EOF"
 sudo sysctl --system
+
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
 
 # install containerd
 sudo apt-get update
@@ -38,9 +52,18 @@ sudo apt-mark hold kubelet kubeadm kubectl
 
 # コントロールプレーンのノードの初期化
 sudo kubeadm config images pull
-sudo kubeadm init
+sudo kubeadm init --pod-network-cidr=10.0.0.0/16 --config kubeadm-config.yaml
+
+# root user 以外でもkubectlコマンド打てるように
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# nodeの確認
 kubectl get node
+
+# calico install
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.1/manifests/tigera-operator.yaml
+wget https://projectcalico.docs.tigera.io/manifests/custom-resources.yaml
+cat custom-resources.yaml | sed 's/192.168.0.0\/24/10.0.0.0\/16/' > custom-resources.yaml
+kubectl apply -f custom-resources.yaml
