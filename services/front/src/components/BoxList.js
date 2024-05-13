a881f6bddc1e91d10843edc8f29dbfddfa621194
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-05-13 22:42:43 +0900

    fix fmt

--- services/front/src/components/BoxList.js
+++ services/front/src/components/BoxList.js
@@ -1,16 +1,23 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import {Link} from 'react-router-dom';
-
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Link } from "react-router-dom";
 
 export default function BoxList(props) {
   return (
-    <List sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper', margin: 'auto', mt: '10px' }}>
+    <List
+      sx={{
+        width: "100%",
+        maxWidth: 500,
+        bgcolor: "background.paper",
+        margin: "auto",
+        mt: "10px",
+      }}
+    >
       {props.filenames.map((value) => {
         const labelId = `checkbox-list-label-${value}`;
 
@@ -18,13 +25,23 @@ export default function BoxList(props) {
           <ListItem
             key={value}
             secondaryAction={
-              <IconButton edge="end" aria-label="comments" onClick={() => props.deleteFunc(value)}>
+              <IconButton
+                edge="end"
+                aria-label="comments"
+                onClick={() => props.deleteFunc(value)}
+              >
                 <DeleteIcon />
               </IconButton>
             }
             disablePadding
           >
-            <ListItemButton component={Link} to={`/chartList/${value}`} role={undefined} dense target="_blank" >
+            <ListItemButton
+              component={Link}
+              to={`/chartList/${value}`}
+              role={undefined}
+              dense
+              target="_blank"
+            >
               <ListItemText align="center" id={labelId} primary={value} />
             </ListItemButton>
           </ListItem>
--- services/front/src/components/Dashboard.js
+++ services/front/src/components/Dashboard.js
@@ -1,27 +1,45 @@
-import React from "react"
+import React from "react";
 
-import config from "../config"
+import config from "../config";
 // import OutlinedCard from "./OutlinedCard"
 
 const Dashboard = () => {
-    const [keepa, setKeepa] = React.useState({})
-    const [mws, setMws] = React.useState({})
+  const [keepa, setKeepa] = React.useState({});
+  const [mws, setMws] = React.useState({});
 
-    React.useEffect(() => {
-        fetch(`${config.fqdn}/api/counts`, {method: "GET", mode: "cors"})
-        .then(res => res.json())
-        .then(data => {
-            setKeepa(data.keepa)
-            setMws(data.mws)
-        })
-    }, [])
-    return (
-        <div>
-            {keepa ? <h3>Keepa Table modified Count: {keepa.modified} / {keepa.total}</h3> : ""}
-            {mws ? <h3>MWS Table price Count: {mws.price} / {mws.total}</h3> : ""}
-            {mws ? <h3>MWS Table fee Count: {mws.fee} / {mws.total}</h3> : ""}
-        </div>
-    )
-}
+  React.useEffect(() => {
+    fetch(`${config.fqdn}/api/counts`, { method: "GET", mode: "cors" })
+      .then((res) => res.json())
+      .then((data) => {
+        setKeepa(data.keepa);
+        setMws(data.mws);
+      });
+  }, []);
+  return (
+    <div>
+      {keepa ? (
+        <h3>
+          Keepa Table modified Count: {keepa.modified} / {keepa.total}
+        </h3>
+      ) : (
+        ""
+      )}
+      {mws ? (
+        <h3>
+          MWS Table price Count: {mws.price} / {mws.total}
+        </h3>
+      ) : (
+        ""
+      )}
+      {mws ? (
+        <h3>
+          MWS Table fee Count: {mws.fee} / {mws.total}
+        </h3>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
