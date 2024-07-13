7264f02622cc500ce43d8ad469a9e2e59e3e4bfc
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-07-13 15:32:24 +0900

    fix update drops command

--- services/api-server/cmd/keepa/main.go
+++ services/api-server/cmd/keepa/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,16 +9,26 @@ import (
 
 	"api-server/database"
 	"api-server/product"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	var asin string
-	var scanningRows bool
+	var cmdUpdateDrops = &cobra.Command{
+		Use: "updateDrops",
+		Run: func(cmd *cobra.Command, args []string) {
+			updateDrops()
+		},
+	}
 
-	flag.StringVar(&asin, "a", "", "start asin")
-	flag.BoolVar(&scanningRows, "s", false, "scanning rows")
-	flag.Parse()
+	var rootCmd = &cobra.Command{
+		Use: "keepa",
+	}
+	rootCmd.AddCommand(cmdUpdateDrops)
+	rootCmd.Execute()
+}
 
+func updateDrops() {
 	dsn := os.Getenv("DB_DSN")
 	if dsn == "" {
 		log.Fatal("dsn null value")
@@ -29,62 +38,43 @@ func main() {
 	ctx := context.Background()
 	wg := &sync.WaitGroup{}
 
-	updateDropsMA := func(keepa []*product.Keepa, wg *sync.WaitGroup) error {
-		defer wg.Done()
-		fmt.Println(keepa[0].Asin)
-		for _, k := range keepa {
-			if err := k.CalculateRankMA(7); err != nil {
-				return err
-			}
+	var asin string
+	var keepas product.Keepas
+	var err error
+	cursor := product.Cursor{
+		End: asin,
+	}
+	limit := 500
+	for {
+		keepas, cursor, err = repo.GetPageNate(context.Background(), cursor.End, limit, product.LoadingData{})
+		if err != nil {
+			log.Print(cursor.End)
+			log.Fatal(err)
 		}
-		if err := repo.UpdateDropsMAV2(ctx, keepa); err != nil {
+		wg.Add(1)
+		go updateDropsMA(ctx, repo, keepas, wg)
+
+		if len(keepas) != limit {
+			log.Print(len(keepas))
+			log.Print("return data len not equal limit")
+			break
+		}
+		log.Print(len(keepas))
+	}
+	wg.Wait()
+	log.Println("done")
+}
+
+func updateDropsMA(ctx context.Context, repo product.KeepaRepository, keepa []*product.Keepa, wg *sync.WaitGroup) error {
+	defer wg.Done()
+	fmt.Println(keepa[0].Asin)
+	for _, k := range keepa {
+		if err := k.CalculateRankMA(7); err != nil {
 			return err
 		}
-		return nil
 	}
-
-	if scanningRows {
-		rows, err := db.NewSelect().Model((*product.Keepa)(nil)).Order("asin").Rows(context.Background())
-		if err != nil {
-			panic(err)
-		}
-		defer rows.Close()
-		for rows.Next() {
-			keepa := new(product.Keepa)
-			if err := db.ScanRow(ctx, rows, keepa); err != nil {
-				panic(err)
-			}
-			if err := updateDropsMA([]*product.Keepa{keepa}, wg); err != nil {
-				panic(err)
-			}
-		}
-		if err := rows.Err(); err != nil {
-			panic(err)
-		}
-	} else {
-		var keepas product.Keepas
-		var err error
-		cursor := product.Cursor{
-			End: asin,
-		}
-		limit := 500
-		for {
-			keepas, cursor, err = repo.GetPageNate(context.Background(), cursor.End, limit, product.LoadingData{})
-			if err != nil {
-				log.Print(cursor.End)
-				log.Fatal(err)
-			}
-			wg.Add(1)
-			go updateDropsMA(keepas, wg)
-
-			if len(keepas) != limit {
-				log.Print(len(keepas))
-				log.Print("return data len not equal limit")
-				break
-			}
-			log.Print(len(keepas))
-		}
-		wg.Wait()
-		log.Println("done")
+	if err := repo.UpdateDropsMAV2(ctx, keepa); err != nil {
+		return err
 	}
+	return nil
 }
--- services/api-server/go.mod
+++ services/api-server/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/labstack/echo/v4 v4.10.2
 	github.com/rabbitmq/amqp091-go v1.9.0
+	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.8.4
 	github.com/uptrace/bun v1.1.14
 	github.com/uptrace/bun/dialect/pgdialect v1.1.14
@@ -26,6 +27,7 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -33,6 +35,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
